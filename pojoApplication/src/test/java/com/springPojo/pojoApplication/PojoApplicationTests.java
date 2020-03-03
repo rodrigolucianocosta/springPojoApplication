87e05ecaee9379c76c2c2c6043765a5fadb6e291
user <user@rodrigo.rlc>
2020-03-03 17:45:54 -0300

    adicao de projeto pojo

---
+++ pojoApplication/src/test/java/com/springPojo/pojoApplication/PojoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springPojo.pojoApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PojoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
